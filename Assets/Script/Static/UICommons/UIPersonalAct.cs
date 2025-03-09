b7cc466e8eae63d45fe9e531cc1a2672eb0d837a
nametakeman <132148860+nametakeman@users.noreply.github.com>
2025-03-10 06:12:51 +0900

    03/10

---
+++ Assets/Script/Static/UICommons/UIPersonalAct.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+/// <summary>
+/// ボタン等のUIそれぞれに存在するクラスの継承元
+/// </summary>
+public class UIPersonalAct : MonoBehaviour
+{
+
+    /// <summary>
+    /// UI移動において例外的な動きをさせたい際のオプション(侵入方向を左からに限定など)
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool MovingException(UIMovingCtrl _ctrl){
+        return false;
+    }
+
+    public virtual void FocusedAction(){
+        
+    }
+
+    public virtual void ClickedAction(){
+
+    }
+}
+
+
+
