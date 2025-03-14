8f33f42c4c0ddc247c31ed7bdf16e3c1fcbc7355
221Fuji <shintarou1030@outlook.com>
2025-03-15 08:19:41 +0900

    2025/03/15

--- Assets/Scripts/Common/ScriptableObject/CharacterData.cs
+++ Assets/Scripts/Common/ScriptableObject/CharacterData.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class CharacterData : ScriptableObject
 {
-    [Header("–¼‘O(‰pŒê–¼)")]
-    [SerializeField] private string _characterName;
+    [Header("‰pŒê–¼")]
+    [SerializeField] private string _characterNameE;
+    [Header("“ú–{–¼")]
+    [SerializeField] private string _characterNameJ;
     [Header("ƒLƒƒƒ‰‚ÌPrefab")]
     [SerializeField] private CharacterActions _characterPrefab;
+    [Header("VersusScene‚Å‚ÌImage")]
+    [SerializeField] private Image _versusEyeForcusImage;
+    [SerializeField] private Image _versusStandingImage;
+    [Header("FightingScene‚Å‚ÌImage")]
+    [SerializeField] private Image _fightingFaceUpImage;
+    [SerializeField] private Image _fightingStandingImage;
 
-    public string CharacterName { get { return _characterName; } }
+
+    public string CharacterNameE { get { return _characterNameE; } }
+    public string CharacterNameJ { get { return _characterNameJ; } }
     public CharacterActions CharacterPrefab { get { return _characterPrefab; } }
+    public Image VersusEyeForcusImage { get {  return _versusEyeForcusImage; } }
+    public Image VersusStandingImage { get { return _versusStandingImage; } }
+    public Image FightingFaceUpImage { get { return _fightingFaceUpImage; } }
+    public Image FightingStandingImage { get { return _fightingStandingImage; } }
 }
