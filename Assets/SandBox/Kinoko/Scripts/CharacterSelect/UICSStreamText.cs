7dea2d91ecda07a5fbbd304ed10ccdc9a41e36dc
nametakeman <132148860+nametakeman@users.noreply.github.com>
2025-03-30 05:35:07 +0900

    0330#4
    メモリリーク修正

--- Assets/SandBox/Kinoko/Scripts/CharacterSelect/UICSStreamText.cs
+++ Assets/SandBox/Kinoko/Scripts/CharacterSelect/UICSStreamText.cs
@@ -10,11 +10,6 @@ public class UICSStreamText : MonoBehaviour
 {
     [SerializeField] private float _endPosition;
     [SerializeField] private float _takeTime;
-    private bool _isDestroy;
-    private void OnDestroy()
-    {
-        _isDestroy = true;
-    }
 
     public async UniTask StreamingText(CancellationToken token)
     {
@@ -23,14 +18,24 @@ public class UICSStreamText : MonoBehaviour
         {
             await GetComponent<RectTransform>().DOAnchorPosX(_endPosition, _takeTime).SetEase(Ease.Linear).ToUniTask(cancellationToken: token);
         }
-        catch { }
-
-        if (_isDestroy) return;
-        GameObject obj = Instantiate(gameObject,firstPos,Quaternion.identity);
-        obj.transform.SetParent(transform.parent,false);
+        catch 
+        {
+            return;
+        }
+        GameObject obj = null;
         try
         {
-            obj.GetComponent<UICSStreamText>().StreamingText(token).Forget();
+
+            obj = Instantiate(gameObject,firstPos,Quaternion.identity);
+        }
+        catch 
+        {
+            return;
+        }
+        obj?.transform.SetParent(transform.parent,false);
+        try
+        {
+            obj?.GetComponent<UICSStreamText>().StreamingText(token).Forget();
         }
         catch{}
         Destroy(gameObject);
